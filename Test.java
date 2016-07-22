97c3282b75c1de0ad0693647c993b52cec00bc1b
FomichevV <fomichev_v@inbox.ru>
2016-07-22 17:27:58 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,9 +1,10 @@
 public class Test{
-public static void main(String[] args){
-int age = 60;
-if (age < 16) {
-System.out.println("Too Yung");}
-else 
-{System.out.println("Welcomeцц!");}
-}
+  public static void main(String[] args){
+    int age = 60;
+    if (age < 16) {
+      System.out.println("Too Yung");}
+    else{
+      System.out.println("Welcomeцц!");
+    }
+  }
 }
